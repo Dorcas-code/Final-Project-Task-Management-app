945c0fdfbcbfb3c55bc0be2dc857b87f6fe09349
Dorcas <55081515+Dorcas-code@users.noreply.github.com>
2026-07-13 08:58:05 +0000

    completed backend

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ dotenv.config();
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
-
 const app = express();
 
 app.use(bodyParser.json());
@@ -17,13 +16,59 @@ app.use(cors());
 app.get("/tasks", (req, res) => {
   (  async () => {
 const dataSourceId =   process.env.NOTION_DB;
-const response = await notion.databases.retrieve({
-  database_id:  dataSourceId});
+const response = await notion.dataSources.query({
+  data_source_id:  dataSourceId,
+});
   console.log(response);
- res.json(response);
+  let final_response = [];
+  response.results.map((item) => {
+    let obj ={
+      id:item.id,
+      name: item.properties.Name.title[0].text.content,
+      status: item.properties.Status.status.name,
+      due_date: item.properties["Due Date"].date.start,
+    }
+    //extract only name, state and due date from the 
+    final_response.push(obj);
+  })
+ res.json(final_response);
     })();
  
 });
+
+app.post("/tasks",async (req, res)=> {
+  let name = req.body.name;
+  let status = req.body.status;
+  let due_date = req.body.due_date;
+  const response = await notion.pages.create({
+    parent: {  data_source_id: process.env.NOTION_DB },
+    properties: {
+       "Name": { "title": [{ "text": { "content": name } }] },
+      "Status": { "status": { "name": status } },
+      "Due Date" : due_date ? { "date": { "start": due_date } }: undefined,
+    },
+  })
+
+  res.json(response);
+})
+
+app.patch("/tasks", async (req, res) => {
+  let id = req.body.id;
+  let status = req.body.status;
+ 
+ const response = await notion.pages.update({
+    page_id: id,
+    properties: {
+    
+      "Status": { "status": { "name": status } },
+     
+    },
+  })
+
+
+  res.json(response);
+})
+
 app.listen(3000, ()=>{
   console.log("server is live")
 })
