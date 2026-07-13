125ba3466b9889c9432cab33ca48bfe7c5d3e5f6
Dorcas <55081515+Dorcas-code@users.noreply.github.com>
2026-07-13 06:34:00 +0000

    server get endpoint link to database

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
 import express from "express";
 import bodyParser from "body-parser";
-import cors from "cors"
-import foxenv from "dotenv"
+import cors from "cors";
+import dotenv from "dotenv";
+import {Client } from "@notionhq/client";
+
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+dotenv.config();
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/tasks", (req, res) => {
+  (  async () => {
+const dataSourceId =   process.env.NOTION_DB;
+const response = await notion.databases.retrieve({
+  database_id:  dataSourceId});
+  console.log(response);
+ res.json(response);
+    })();
+ 
+});
+app.listen(3000, ()=>{
+  console.log("server is live")
+})
+
