892a1065cb0e643eda37cd35af43b37442c647f9
Dorcas <55081515+Dorcas-code@users.noreply.github.com>
2026-07-13 09:42:23 +0000

    front end simple form

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      minHeight: "100vh",
+      backgroundColor: "#effcff",
+    }}>
+      {/* form to submit tasks */}
+      <h1>Progress Tracker</h1>
+      <form style={{
+        display: "flex",
+        gap: "10px",}}>
+        <input type="text" style={{ height: "30px" ,fontSize:"16px",borderRadius:"5px" }} placeholder="Task name"></input>
+        <input type="date"  style={{ height: "30px" ,fontSize:"16px",borderRadius:"5px"} }></input>
+        <button  style={{ height: "30px " ,fontSize:"16px",borderRadius:"5px" }} >Add Task</button>
+      </form>
     </div>
   );
 }
