7922891dc88e67a3fb7495d94ed70e49cc8f898e
Dorcas <55081515+Dorcas-code@users.noreply.github.com>
2026-07-16 06:42:41 +0000

    added draggable function

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [date, setDate] = useState("");
  
     // With this:
-  const codespaceName = "fantastic-yodel-pw79469x94wh7gg";
+  const codespaceName = "zany-trout-5rv5j95wrr737pr5";
   const port = 3000; // Your actual port
   const domain = "app.github.dev";
    const url = `https://${codespaceName}-${port}.${domain}/tasks`;
@@ -16,7 +16,7 @@ function App() {
  const fetchTasks = async() => {
 
   // With this:
-const codespaceName = "fantastic-yodel-pw79469x94wh7gg";
+const codespaceName = "zany-trout-5rv5j95wrr737pr5";
 
 const port = 3000; // Your actual port
 const domain = "app.github.dev";
@@ -67,8 +67,25 @@ const domain = "app.github.dev";
     // console.log("Task Date:", date);
   }
 
- 
+ const handleDragStart = (e,id) => {
+  e.dataTransfer.setData("text/plain", String(id))
+  //
+ }
 
+  const handleDrop = async(e, status) => {
+   e.preventDefault();
+   const id = e.dataTransfer.getData("text/plain");
+   //get data in format of plain text
+   console.log(id, status);
+     await fetch(url, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ id:id, status:status}) // Stringify the payload
+  });
+    fetchTasks();
+  }
 
   return (
     <div style={{
@@ -89,12 +106,14 @@ const domain = "app.github.dev";
         <button  style={{ height: "30px " ,fontSize:"16px",borderRadius:"5px" }} >Add Task</button>
       </form>
       <div style={{ display: "flex", justifyContent:"center",gap:"30px", width:"100%"}}>
-        <div style={{backgroundColor: "White",  borderRadius: "15px", minHeight:"300px",width:"300px",marginTop:"10px",padding:"15px"}}>
+        <div onDragOver={(e)=>e.preventDefault()} 
+             onDrop={(e)=> handleDrop(e, "Not started")}
+              style={{backgroundColor: "White",  borderRadius: "15px", minHeight:"300px",width:"300px",marginTop:"10px",padding:"15px"}}>
           <h2 style={{textAlign: "center"}}>Not Started</h2>
           {tasks.map((task) => {
             if(task.status === "Not started"){
               return (
-                <div key={task.id} style={{backgroundColor: "#fef2f2", borderRadius: "5px", padding: "10px", marginBottom: "10px", cursor: "pointer"}}>
+                <div draggable onDragStart={(e)=> handleDragStart(e,task.id)} key={task.id} style={{backgroundColor: "#fef2f2", borderRadius: "5px", padding: "10px", marginBottom: "10px", cursor: "pointer"}}>
                   {console.log(task.id)}<strong>{task.name} </strong> {console.log(task.name)}
          <span>Due Date: {task.due_date}</span>
 
@@ -103,24 +122,28 @@ const domain = "app.github.dev";
             }})}
         </div>
         
-         <div style={{backgroundColor: "White",  borderRadius: "15px", minHeight:"300px",width:"300px",marginTop:"10px",padding:"15px"}}>
+         <div onDragOver={(e)=>e.preventDefault()} 
+             onDrop={(e)=> handleDrop(e, "In progress")}
+             style={{backgroundColor: "White",  borderRadius: "15px", minHeight:"300px",width:"300px",marginTop:"10px",padding:"15px"}}>
           <h2 style={{textAlign: "center"}}>In Progress</h2>
           {tasks.map((task) => {
             if(task.status === "In progress"){
               return (
-                <div style={{backgroundColor: "#fef2f2", borderRadius: "5px", padding: "10px", marginBottom: "10px", cursor: "pointer"}}>
+                <div draggable onDragStart={(e)=> handleDragStart(e,task.id)}  style={{backgroundColor: "#fef2f2", borderRadius: "5px", padding: "10px", marginBottom: "10px", cursor: "pointer"}}>
                   <strong>{task.name} </strong>
                   <span>Due Date: {task.due_date}</span>
                 </div>
               )
             }})}
         </div>
-        <div style={{backgroundColor: "White",  borderRadius: "15px", minHeight:"300px",width:"300px",marginTop:"10px",padding:"15px"}}>
+        <div onDragOver={(e)=>e.preventDefault()} 
+             onDrop={(e)=> handleDrop(e, "Done")}
+             style={{backgroundColor: "White",  borderRadius: "15px", minHeight:"300px",width:"300px",marginTop:"10px",padding:"15px"}}>
           <h2 style={{textAlign: "center"}}>Completed</h2>
           {tasks.map((task) => {
             if(task.status === "Done"){
               return (
-                <div style={{backgroundColor: "#fef2f2", borderRadius: "5px", padding: "10px", marginBottom: "10px", cursor: "pointer"}}>
+                <div draggable onDragStart={(e)=> handleDragStart(e,task.id)}  style={{backgroundColor: "#fef2f2", borderRadius: "5px", padding: "10px", marginBottom: "10px", cursor: "pointer"}}>
                   <strong>{task.name} </strong>
                   <span>Due Date: {task.due_date}</span>
                 </div>
